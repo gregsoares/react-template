9eda681d613c97599081bd162fb4c45bd742a3da
Yggdrasil <gregselvagem@gmail.com>
2022-02-06 11:21:49 -0500

    clear react template first commit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,15 @@
+import Header from '../../components/Atoms/Header'
+import React from 'react'
+
+import './Home.css'
+const Home = () => {
+  return (
+    <main>
+      {' '}
+      <h1 className='title'>Home Page</h1>
+      <Header>Header</Header>
+    </main>
+  )
+}
+
+export default Home
